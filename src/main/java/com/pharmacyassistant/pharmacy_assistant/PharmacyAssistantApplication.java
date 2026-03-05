25641a412fa9d80724e78f3025c47d6ababfdb06
mrnrdgz <mrnrdgz@gmail.com>
2026-03-05 16:15:05 -0300

    chore: bootstrap Spring Boot project and environment setup

---
+++ src/main/java/com/pharmacyassistant/pharmacy_assistant/PharmacyAssistantApplication.java
@@ -0,0 +1,13 @@
+package com.pharmacyassistant.pharmacy_assistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PharmacyAssistantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PharmacyAssistantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pharmacyassistant/pharmacy_assistant/PharmacyAssistantApplicationTests.java
@@ -0,0 +1,12 @@
+package com.pharmacyassistant.pharmacy_assistant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class PharmacyAssistantApplicationTests {
+    @Test
+    void contextLoads() {}
+}
